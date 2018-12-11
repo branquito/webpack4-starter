9c8cf2bac9e7bfd4f83d317ffe981cab76be141f
branchito <branko@renheadtechnology.com>
2018-12-11 16:11:53 +0100

    Separate files per build, from two different sources
    now we have index.js and index_alt.js, so we can make two separate single page apps
    this is good for having separated contexts when trying out things, as each can have
    it’s own set of components along with different css frameworks if needed

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,59 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin")
-const MiniCssExtractPlugin = require("mini-css-extract-plugin")
-const VueLoaderPlugin = require("vue-loader/lib/plugin")
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const path = require('path')
 module.exports = {
+  entry: {
+    page1: './src/index.js',
+    page2: './src/index_alt.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js'
+  },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
+          loader: 'babel-loader'
         }
       },
       {
         test: /\.html$/,
         use: {
-          loader: "html-loader",
-          options: {minimize: true}
+          loader: 'html-loader',
+          options: { minimize: true }
         }
       },
       {
         test: /\.vue/,
         exclude: /node_modules/,
         use: {
-          loader: "vue-loader"
+          loader: 'vue-loader'
         }
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"]
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
       }
     ]
   },
   plugins: [
     new VueLoaderPlugin(),
     new HtmlWebPackPlugin({
-      template: "./src/index.html"
+      chunks: ['page1'],
+      template: './src/index.html'
+    }),
+    new HtmlWebPackPlugin({
+      chunks: ['page2'],
+      template: './src/index.html',
+      filename: 'index_alt.html'
     }),
     new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css"
+      filename: '[name].css',
+      chunkFilename: '[id].css'
     })
   ]
 }
