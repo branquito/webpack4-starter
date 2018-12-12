68d03c9ccec76cb870d255ae7bf19b80a1b6d0c7
Branchito de Munze <brankompas@gmail.com>
2018-12-12 20:35:00 +0100

    Add form example

--- src/App3.vue
+++ src/App3.vue
@@ -1,9 +1,22 @@
-<template><div>vue app 3</div></template>
+<template>
+  <form-example :frmModel="frmModel"></form-example>
+</template>
 <script>
+import FormExample from "./components/FormExample.vue"
 export default {
   name: "app3",
+  components: {FormExample},
   data() {
-    return {}
+    return {
+      frmModel: {
+        name: {
+          type: "text"
+        },
+        age: {
+          type: "text"
+        }
+      }
+    }
   }
 }
 </script>
---
+++ src/components/FormExample.vue
@@ -0,0 +1,25 @@
+<template>
+  <ul>
+    <li v-for="control in controls">{{ control }}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    frmModel: {
+      type: Object,
+      required: true,
+      default: () => {}
+    }
+  },
+  computed: {
+    controls() {
+      return Object.entries(this.frmModel)
+    }
+  },
+  data() {
+    return {}
+  }
+}
+</script>
