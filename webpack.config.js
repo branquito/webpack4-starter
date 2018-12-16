1d86328943f566ae3a03fdb1bfa77dc8506d8911
Branchito de Munze <brankompas@gmail.com>
2018-12-16 18:08:25 +0100

    Add sass loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
-const HtmlWebPackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const VueLoaderPlugin = require('vue-loader/lib/plugin')
-const path = require('path')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const VueLoaderPlugin = require("vue-loader/lib/plugin")
+const path = require("path")
 module.exports = {
   entry: {
-    page1: './src/index.js',
-    page2: './src/index_alt.js'
+    page1: "./src/index.js",
+    page2: "./src/index_alt.js"
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].bundle.js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js"
   },
   module: {
     rules: [
@@ -17,43 +17,43 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
+          loader: "babel-loader"
         }
       },
       {
         test: /\.html$/,
         use: {
-          loader: 'html-loader',
-          options: { minimize: true }
+          loader: "html-loader",
+          options: {minimize: true}
         }
       },
       {
         test: /\.vue/,
         exclude: /node_modules/,
         use: {
-          loader: 'vue-loader'
+          loader: "vue-loader"
         }
       },
       {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
+        test: /\.(sa|sc|c)ss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
       }
     ]
   },
   plugins: [
     new VueLoaderPlugin(),
     new HtmlWebPackPlugin({
-      chunks: ['page1'],
-      template: './src/index.html'
+      chunks: ["page1"],
+      template: "./src/index.html"
     }),
     new HtmlWebPackPlugin({
-      chunks: ['page2'],
-      template: './src/index_alt.html',
-      filename: 'index_alt.html'
+      chunks: ["page2"],
+      template: "./src/index_alt.html",
+      filename: "index_alt.html"
     }),
     new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: '[id].css'
+      filename: "[name].css",
+      chunkFilename: "[id].css"
     })
   ]
 }
