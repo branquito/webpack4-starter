a14759e61071a35051f0376c76ee9c55ac66774c
brachito <branchito@Branchitos-MacBook-Pro.local>
2019-03-22 17:45:50 +0100

    YesNo type of question, add fields

--- src/questions/components/types/YesNoQuestion.vue
+++ src/questions/components/types/YesNoQuestion.vue
@@ -26,7 +26,42 @@
         </template>
       </div>
       <div v-else>No options</div>
-      <div class="col-xs-6">Picked: <pre class="text-xs bg-yellow-lightest">{{ Q.picked }}</pre></div>
+
+      <hr class="border">
+
+      <div class="md:flex md:items-start my-6">
+        <div class="md:w-1/3">
+          <label class="text-sm font-medium text-rn-grey-dark-label">Confidentiality</label>
+        </div>
+        <div class="md:w-2/3">
+          <input type="checkbox" v-model="Q.isConfidential"> <span class="text-xs">Make this question confidential</span>
+        </div>
+      </div>
+
+      <!-- TODO make separate FormInput for tags autocompletion -->
+      <FormInput v-model="Q.tags" label="Category"></FormInput>
+
+      <div class="md:flex md:items-start my-6">
+        <div class="md:w-1/3">
+          <label class="text-sm font-medium text-rn-grey-dark-label">Answer</label>
+        </div>
+        <div class="md:w-2/3">
+          <input type="checkbox" v-model="Q.addPredefined"> <span class="text-xs">I want to add preferred answer</span>
+        </div>
+      </div>
+
+      <div v-if="Q.addPredefined" class="md:flex md:items-start mb-6">
+        <div class="md:w-1/3">
+          <label class="text-sm font-medium text-rn-grey-dark-label">Add predefined answer</label>
+        </div>
+        <div class="md:w-2/3">
+          <textarea class="border rounded rounded-sm" v-model="Q.answer"></textarea>
+        </div>
+      </div>
+      <div class="md:flex md:items-center mb-6">
+        <pre class="text-xs bg-yellow-lightest">{{ Q.options }}</pre>
+        <pre class="text-xs bg-yellow-lightest">{{ Q.picked }}</pre>
+      </div>
       <div class="md:w-1/3"></div>
       <div class="md:w-2/3">
         <!-- // todo cancel -->
